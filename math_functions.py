d7cc723cc87296a0e15e7e64983360807bf69e36
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-06-24 15:17:02 +0200

    Math functions -- poisson thingy

--- math_functions.py
+++ math_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import gamma
 
 def hill_equation(k, n):
     def _hill_function(x):
@@ -10,3 +11,6 @@ def hill_equation(k, n):
 
 def uptake_function(x, A = 1.0, K = 1.0, L = 1.0):
     return  A*x/(K + x*(1 + x/L))
+
+def uptake_poisson(x, A=1.0, l=1.0):
+    return A*(np.exp(-l) * l**x)/gamma(x+1)
